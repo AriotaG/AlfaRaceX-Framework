54f986b0e75c266899bc3bcb64cfe134daf9322b
Giuseppe Ariota <58816127+AriotaG@users.noreply.github.com>
2026-09-23 20:27:55 +0200

    updater: show branded startup splash

--- updater/AlfaRaceX.Updater/Program.cs
+++ updater/AlfaRaceX.Updater/Program.cs
@@ -6,6 +6,22 @@ internal static class Program
     private static void Main()
     {
         ApplicationConfiguration.Initialize();
+
+        using (var splash = new SplashForm())
+        {
+            splash.Show();
+            splash.Refresh();
+
+            var until = DateTime.UtcNow.AddMilliseconds(1450);
+            while (DateTime.UtcNow < until)
+            {
+                Application.DoEvents();
+                Thread.Sleep(15);
+            }
+
+            splash.Close();
+        }
+
         Application.Run(new MainForm());
     }
 }
